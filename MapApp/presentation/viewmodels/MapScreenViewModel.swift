ab5b03674e8a55c80aabee3a67d8257a42dc6842
tharinzaman <107755507+tharinzaman@users.noreply.github.com>
2024-02-19 15:37:34 +0000

    WelcomeScreen -> MapScreen navigation added - needs parameter to determine user vs random location fetch

--- MapApp/presentation/viewmodels/MapScreenViewModel.swift
+++ MapApp/presentation/viewmodels/MapScreenViewModel.swift
@@ -1,49 +1,8 @@
 //
-//  MapViewModel.swift
+//  MapScreenViewModel.swift
 //  MapApp
 //
 //  Created by Tharin Zaman on 19/02/2024.
 //
 
-import MapKit
-
-final class MapScreenViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
-    
-    private let locationHelper: LocationHelper
-    
-    init(locationHelper: LocationHelper) {
-        self.locationHelper = locationHelper
-    }
-    
-    @Published var region = MKCoordinateRegion(
-        center: Constants.defaultLocation,
-        span: Constants.span
-    )
-    
-    func checkLocationServices() {
-        locationHelper.checkIfLocationServicesAreEnabled()
-    }
-    
-    func checkLocationPermissions() {
-        if locationHelper.checkIfLocationPermissionsAreGranted() {
-            if let userLocation = locationHelper.getUserLocation() {
-                self.region = userLocation
-            }
-        }
-    }
-    
-}
-
-// MARK: Extension for delegate methods
-extension MapScreenViewModel {
-    
-    func locationManager(
-        _ manager: CLLocationManager,
-        didChangeAuthorization status: CLAuthorizationStatus
-    ) {
-        checkLocationPermissions()
-    }
-    
-}
-
-
+import Foundation
---
+++ MapApp/presentation/viewmodels/WelcomeScreenViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  MapViewModel.swift
+//  MapApp
+//
+//  Created by Tharin Zaman on 19/02/2024.
+//
+
+import MapKit
+
+final class WelcomeScreenViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    private let locationHelper: LocationHelper
+    
+    init(locationHelper: LocationHelper) {
+        self.locationHelper = locationHelper
+    }
+    
+    @Published var region = MKCoordinateRegion(
+        center: Constants.defaultLocation,
+        span: Constants.span
+    )
+    
+    func checkLocationServices() {
+        locationHelper.checkIfLocationServicesAreEnabled()
+    }
+    
+    func checkLocationPermissions() {
+        if locationHelper.checkIfLocationPermissionsAreGranted() {
+            if let userLocation = locationHelper.getUserLocation() {
+                self.region = userLocation
+            }
+        }
+    }
+    
+}
+
+// MARK: Extension for delegate methods
+extension WelcomeScreenViewModel {
+    
+    func locationManager(
+        _ manager: CLLocationManager,
+        didChangeAuthorization status: CLAuthorizationStatus
+    ) {
+        checkLocationPermissions()
+    }
+    
+}
+
+
